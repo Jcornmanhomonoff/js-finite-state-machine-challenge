964a784f2e014708f258c5fc1d0e0deec6522fdb
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2017-06-06 13:18:15 -0400

    Add tapCard & insertTicket methods
    Add tapCard & insertTicket methods to challenge as well as tests for
    the methods

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -17,15 +17,26 @@ describe('Finite State Machine', function () {
     expect(gate.state).to.equal('closed')
   })
 
-  it('has a tapCard method that transitions state to open', function () {
+  it('has a tapCard method that transitions state to open if the Charlie Card has value', function () {
+    expect(gate.tapCard()).to.equal(true)
     gate.tapCard()
     expect(gate.tapCard).to.be.a.function
     expect(gate.state).to.equal('open')
+    gate.walkThrough()
+  })
+
+  it('has an insertTicket method that transitions state to open if the Charlie Ticket has a value greater or equal to 2.25', function () {
+    expect(gate.insertTicket).to.be.a.function
+    if (gate.insertTicket()) {
+      expect(gate.state).to.equal('open')
+    } else {
+      expect(gate.state).to.equal('closed')
+    }
+    gate.walkThrough()
   })
 
   it('has a walkThrough method that transitions state to closed if state is currently open', function () {
     gate.tapCard()
-    expect(gate.walkThrough()).to.equal(true)
     gate.walkThrough()
     expect(gate.walkThrough).to.be.a.function
     expect(gate.state).to.equal('closed')
